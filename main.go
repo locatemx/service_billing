940914f8d41c1af1c376a98d56ee2dc06a4eddfd
Gerardo Juárez Ramírez <gerardo.juarez.ramirez@outlook.com>
2025-01-03 15:28:22 -0600

    main better version

--- main.go
+++ main.go
@@ -9,21 +9,36 @@ import (
 	"github.com/joho/godotenv"
 )
 
-func main() {
+func loadEnv() {
+	// Carga el archivo .env
 	err := godotenv.Load()
 	if err != nil {
-		log.Fatalf("Error cargando el archivo .env: %v", err)
+		log.Println("Advertencia: No se pudo cargar el archivo .env, se usarán las variables de entorno.")
 	}
+}
 
+func connectDatabase() {
+	// Obtener la cadena de conexión desde las variables de entorno
 	connectionString := os.Getenv("PG_CONNECTION")
 	if connectionString == "" {
-		log.Fatal("Error: La variable PG_CONNECTION no está definida")
+		log.Fatal("Error: La variable PG_CONNECTION no está definida.")
 	}
 
+	// Conectar a la base de datos
 	db.ConnectDB(connectionString)
+	log.Println("Conexión a PostgreSQL completada.")
+}
+
+func main() {
+	// Cargar variables de entorno
+	loadEnv()
+
+	// Conectar a la base de datos
+	connectDatabase()
 	defer db.DB.Close()
 
-	log.Println("Conexión a PostgreSQL completada. Aplicación iniciada.")
+	// Iniciar servicios
+	log.Println("Aplicación iniciada.")
 	services.GenerateInvoices()
-	log.Println("Facturación completada.")
+	log.Println("Facturación completada con éxito.")
 }
